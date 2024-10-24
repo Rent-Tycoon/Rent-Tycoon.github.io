812d9653423b8dfab33854f1066f216a96c8578e
Alexander Ivanisov <info@rispo.ru>
2024-10-24 09:47:02 +0300

    Ready

--- nft/src/make_1.py
+++ nft/src/make_1.py
@@ -1,6 +1,27 @@
+# lottery-bronze.json - 25
+# lottery-silver.json - 15
+# lottery-gold.json - 9
 # lottery-golden_ticket.json - 1
 
-# lottery-bronze.json - 25
+import json
 
-# lottery-silver.json - 15
+def replace_and_save(template_file, placeholder, count, start):
+    with open(f"../meta/tmpl/{template_file}", 'r', encoding='utf-8') as file:
+        template = json.load(file)
 
+    for i in range(start, start + count):
+        output_file = f"nft_{i}.json"
+        
+        modified_template = json.loads(json.dumps(template).replace(placeholder, str(i)))
+
+        output_file = f"../meta/{output_file}"
+        with open(output_file, 'w', encoding='utf-8') as file:
+            json.dump(modified_template, file, ensure_ascii=False, indent=4)
+
+        print(f"file saved: {output_file}")
+
+# Process
+replace_and_save('lottery-bronze.json', '{#?}', 25, 0)
+replace_and_save('lottery-silver.json', '{#?}', 15, 25)
+replace_and_save('lottery-gold.json', '{#?}', 9, 25 + 15)
+replace_and_save('lottery-golden_ticket.json', '{#?}', 1, 25 + 15 + 9)
