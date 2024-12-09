a2afe4660336885585fb35a7115e521fdbe2b8d7
Alexander Ivanisov <info@rispo.ru>
2024-12-09 12:47:30 +0300

    progress

--- nft/src/make_3_fake.py
+++ nft/src/make_3_fake.py
@@ -1,3 +1,3 @@
 from lib import meta_generate
 
-meta_generate.replace_and_save('item.json', '{#?}', 5, 0, 0, 'meta_fake')
+meta_generate.replace_and_save('item.json', '{#?}', 10, 0, 0, 'meta_fake')
