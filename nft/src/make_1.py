677293985b7c4b0c5d0728b0fbc2be042f90aae2
Alexander Ivanisov <info@rispo.ru>
2025-01-09 21:11:11 +0300

    lotterey update

--- nft/src/make_1.py
+++ nft/src/make_1.py
@@ -12,7 +12,18 @@ from lib import meta_generate
 # meta_generate.replace_and_save('lottery-golden_ticket.json', '{#?}', 1, 25 + 15 + 9)
 
 # Rounde 2 ( 50+ )
-meta_generate.replace_and_save('lottery-gold_r2.json', '{#?}', 9, 50)
-meta_generate.replace_and_save('lottery-bronze_r2.json', '{#?}', 65, 50 + 9)
-meta_generate.replace_and_save('lottery-silver_r2.json', '{#?}', 25, 50 + 9 + 65)
-meta_generate.replace_and_save('lottery-golden_ticket.json', '{#?}', 1, 50 + 9 + 65 + 25, 1)
+# meta_generate.replace_and_save('lottery-gold_r2.json', '{#?}', 9, 50)
+# meta_generate.replace_and_save('lottery-bronze_r2.json', '{#?}', 65, 50 + 9)
+# meta_generate.replace_and_save('lottery-silver_r2.json', '{#?}', 25, 50 + 9 + 65)
+# meta_generate.replace_and_save('lottery-golden_ticket.json', '{#?}', 1, 50 + 9 + 65 + 25, 1)
+
+
+round2_end = 50 + 9 + 65 + 25 + 1
+print(f'round2_end: {round2_end}')
+
+# Rounde 3 ( 50+ )
+meta_generate.replace_and_save('lottery-golden_ticket.json', '{#?}', 1, round2_end, 2)
+meta_generate.replace_and_save('lottery-gold_r3.json', '{#?}', 9, round2_end + 1)
+meta_generate.replace_and_save('lottery-silver_r3.json', '{#?}', 25, round2_end + 1 + 9)
+meta_generate.replace_and_save('lottery-bronze_r3.json', '{#?}', 65, round2_end + 1 + 9 + 25) 
+
