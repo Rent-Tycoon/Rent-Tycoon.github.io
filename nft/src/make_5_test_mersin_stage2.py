fd49d44a6b97c99d8636161afcda74e167d88096
Alexander Ivanisov <info@rispo.ru>
2025-01-14 11:05:29 +0300

    progress

--- nft/src/make_5_test_mersin_stage2.py
+++ nft/src/make_5_test_mersin_stage2.py
@@ -1,3 +1,3 @@
 from lib import meta_generate
 
-meta_generate.replace_and_save('item.json', '{#?}', 3, 0, 0, 'meta_metsin_test_2')
+meta_generate.replace_and_save('item.json', '{#?}', 3, 0, 0, 'meta_mersin_test_2')
